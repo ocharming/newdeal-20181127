eb117c483c46933822a7104e6d996f07fb5f7be4
ohcharming <ocm3323@gmail.com>
2018-11-29 10:45:12 +0900

    v04

---
+++ newdeal-project-04/src/main/java/Lesson.java
@@ -0,0 +1,17 @@
+import java.util.Date;
+
+// 새 데이터 타입
+// 새로운 종류의 메모리를 구성하는 설계도가 클래스다.
+
+public class Lesson {
+
+  //인스턴스 필드 데이터를 개별적으로 다룰때는 인스턴스 변수로 만든다.
+  int no;
+  String title;
+  String contents;
+  Date startDate;
+  Date endDate;
+  int totalHours;
+  int dayHours;
+
+}
